75deedce150e72e1ffc9b1f16b303d477176ce2f
ELYN-coder <123931935+ELYN-coder@users.noreply.github.com>
2023-02-02 14:12:43 +0800

    add main file

---
+++ main.py
@@ -0,0 +1,15 @@
+# import modules
+import cash_on_hand, overheads, profit_loss
+
+# define main function
+def main():
+    """
+    - This function contains functions created for all 3 datas
+    """
+
+    overheads.overheads_function()
+    cash_on_hand.coh_function()
+    profit_loss.profitloss_function()
+
+# execute the main function
+main()
